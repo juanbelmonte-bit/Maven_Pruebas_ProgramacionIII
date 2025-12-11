7126e361671a160e7ec8760d4427d431215ab6ca
alumno <alumno@usal.es>
2025-12-11 20:08:29 +0100

    Actualizacion del ejercicio.

---
+++ pruebamaven/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.belmontegonzalez","l":"Main"},{"p":"com.belmontegonzalez","l":"Persona"}];updateSearchResults();
