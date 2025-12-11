a5b6033dd5c5b848616dd7737d38a9aba57e4a04
alumno <alumno@usal.es>
2025-12-11 19:44:48 +0100

    Creado el proyecto maven

--- pruebamaven/src/main/java/com/belmontegonzalez/Main.java
+++ pruebamaven/src/main/java/com/belmontegonzalez/Main.java
@@ -1,7 +1,13 @@
 package com.belmontegonzalez;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona = new Persona("Juan", "Belmonte", 21, "juanbelmonte@usal.es");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(persona);
+        System.out.println(json);
     }
 }
